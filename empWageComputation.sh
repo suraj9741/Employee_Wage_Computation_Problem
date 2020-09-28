ccba4bd89b36654582d7bd68340314adb67bf122
Suraj <surajjadhav9741@gmail.com>
2020-09-29 01:53:42 +0530

    Calcilate Wages for a Month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,6 +2,7 @@
 echo "Welcome to Employee Wage Computation Program"
 a=$((RANDOM%3))
 Wage_per_hour=20
+Total_Month_Wage=0
 Day_hour=8
 Part_time=4
 Total_Wage=1
@@ -11,19 +12,17 @@ do
 	a=$((RANDOM%3))
 	case $a in
 		1)
-			echo "Present Full time employee"
 			empHrs=8
 		;;
 		2)
-			echo "Present Part time employee"
 			empHrs=4
 		;;
 		*)
-			echo "Absent"
 			empHrs=0
 		;;
 	esac
 		Total_Wage=$(($empHrs*$Wage_per_hour))
-		echo "$day day $Total_Wage"
+		Total_Month_Wage=$((Total_Month_Wage+Total_Wage))
 	day=$((day+1))
 done
+echo "Total Month Wages : $Total_Month_Wage"
