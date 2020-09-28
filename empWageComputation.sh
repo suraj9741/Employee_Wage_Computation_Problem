a85acfc56111d49e15b441d2cabd84a908b24003
Suraj <surajjadhav9741@gmail.com>
2020-09-29 00:42:21 +0530

    Add Part time

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,14 +1,21 @@
 ##!/bin/bash -x
 echo "Welcome to Employee Wage Computation Program"
-a=$((RANDOM%2))
+a=$((RANDOM%3))
 Wage_per_hour=20
 Day_hour=8
+Part_time=4
 Total_Wage=1
 if [ $a -eq 0 ]
 then
-	echo "Present"
+	echo "Present Full time employee"
 	Total_Wage=$(($Wage_per_hour*$Day_hour))
 	echo "Total Wage : " $Total_Wage
+elif [ $a -eq 1 ]
+then
+	echo "Present Part time employee"
+        Total_Wage=$(($Wage_per_hour*$Part_time))
+        echo "Total Wage : " $Total_Wage
+
 else
 	echo "Absent"
 	Total_Wage=0
