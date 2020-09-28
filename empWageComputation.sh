c7f3ec9819daf9407117849a7cd1e185231b4a13
Suraj <surajjadhav9741@gmail.com>
2020-09-29 04:14:17 +0530

    Store the Day and the Daily Wage along with the Total Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -8,20 +8,26 @@ Part_time=4
 Total_Wage=1
 day=1
 hours=0
+declare -A PA
 while (($(($day<21))&&$(($hours<=100))))
 do
 	a=$((RANDOM%3))
 	case $a in
 		1)
 			empHrs=8
+			P="Present"
 		;;
 		2)
 			empHrs=4
+			P="Present"
 		;;
 		*)
 			empHrs=0
+			P="Absent "
 		;;
 	esac
+		empHrstemp[$((day-1))]=$empHrs
+		PA[$((day-1))]=$P
 		hours=$((hours+empHrs))
 		Total_Wage=$(($empHrs*$Wage_per_hour))
 		Total_Month_Wage=$((Total_Month_Wage+Total_Wage))
@@ -30,8 +36,9 @@ do
 done
 echo "Total Month Wages : $Total_Month_Wage"
 echo "Total hours : $hours"
+echo "Day           Present/Absent           Work Hours       Total Month Wage"
 for((i=0;i<${#Month_Wage[@]};i++))
 do
-	echo "$((i+1)) : ${Month_Wage[$i]}"
+	echo "$((i+1))               ${PA[$i]}                   ${empHrstemp[$i]}                   ${Month_Wage[$i]}"
 done
-#echo "${Month_Wage[@]}"
+
